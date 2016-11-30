2da9eeb5bc0c1e6aa3093d408040515a7b0ba95f
Aras Abdo <route_zero@hotmail.com>
2016-11-30 15:39:39 +0000

    version 5 - lab5

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,5 +10,16 @@ class HomeControllerTest < ActionController::TestCase
     assert_select 'h1', 'My Notes'
     assert_select 'p', 'Welcome to my Notes Site'
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    
+    assert_template layout: 'application'
+    
+    assert_select 'title', 'My Notes'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us'
+  end
 
 end
